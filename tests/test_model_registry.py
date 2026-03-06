cd0ee8fa22853c3e45c111e634320b9e9b7cccab
Avish Jha <avish.j@protonmail.com>
2026-03-06 00:11:50 -0500

    test: rm useless test
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- tests/test_model_registry.py
+++ tests/test_model_registry.py
@@ -6,12 +6,6 @@ from autocustomizeresume.model_registry import _DEFAULTS, get_model_params
 
 
 class TestGetModelParams:
-    def test_known_model_overrides_defaults(self):
-        params = get_model_params("qwen/qwen3.5-397b-a17b")
-        # A registered model should differ from defaults in at least one key
-        assert any(params[k] != _DEFAULTS[k] for k in _DEFAULTS)
-        # And still contain all expected keys
-        assert set(_DEFAULTS) <= set(params)
 
     def test_unknown_model_returns_defaults(self):
         assert get_model_params("some/unknown-model") == _DEFAULTS
