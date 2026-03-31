fb4325b71b34fd4d1453740fb2bdf92a1af82aea
Avish Jha <avish.j@pm.me>
2026-03-31 16:11:42 -0400

    chore: sync spdx headers
    Signed-off-by: Avish Jha <avish.j@pm.me>

--- src/autocustomizeresume/__main__.py
+++ src/autocustomizeresume/__main__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Avish Jha <avish.j@pm.me>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 """Allow running as `python -m autocustomizeresume`."""
 
 from autocustomizeresume.cli import app
